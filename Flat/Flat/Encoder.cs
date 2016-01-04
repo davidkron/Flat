c4fe1ccd8152c1ede6ecedb51406af052ecfce9f
David Jensen <david_jensen3@hotmail.com>
2016-01-04 23:36:13 +0100

    Basic encode support

---
+++ Flat/Flat/Encoder.cs
@@ -0,0 +1,45 @@
+﻿// This project can output the Class library as a NuGet Package.
+// To enable this option, right-click on the project and select the Properties menu item.
+//In the Build tab select "Produce outputs on build".
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Flat
+{
+    public static class Encoder
+    {
+
+        public delegate IEnumerable<T> GetChildren<T>(T parent);
+        public delegate IEnumerable<string> GetDataList<in T>(T parent);
+        
+
+        public static string Flatten<T>(this IEnumerable<T> nodes,
+        GetChildren<T> childAccecor,Func<T,string> nameAccessor,GetDataList<T> dataAccessor,string path = "")
+        {
+            var output = "";
+            foreach (var node in nodes.OrderBy(nameAccessor))
+            {
+                output += EncodeSingleNode(path + nameAccessor(node));
+                var childNodes = childAccecor(node);
+                var newPath = path;
+                if (newPath.Any())
+                    newPath += "\\";
+                newPath += nameAccessor(node) + "\\";
+                output += childNodes.Flatten(childAccecor, nameAccessor, dataAccessor, newPath);
+            }
+            return output;
+        }
+
+        public static string EncodeSingleNode(string name)
+        {
+            return "[" + name + "]" + "\n";
+        }
+
+        public static string FlattenNodeName(string name, string path)
+        {
+            return path + "\\" + name;
+        }
+    }
+}
---
+++ Flat/Tests/EncoderTests.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Flat;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class EncoderTests
+    {
+        class TestNode
+        {
+            public string Name { get; set; }
+            public TestNode(string name)
+            {
+                Name = name;
+            }
+
+            public TestNode()
+            {
+            }
+
+            public List<TestNode> Children { get; } = new List<TestNode>();
+            public List<string> Data { get; set; }
+        }
+
+        [TestMethod]
+        public void EncodeTwoRootEntrys()
+        {
+            var root = new TestNode();
+            root.Children.Add(new TestNode("Lib"));
+            root.Children.Add(new TestNode("Client" ));
+            var res = root.Children.Flatten(
+                parent => parent.Children,
+                parent => parent.Name,
+                parent => parent.Data).Split('\n');
+            
+            Assert.AreEqual("[Client]", res[0]);
+            Assert.AreEqual("[Lib]", res[1]);
+        }
+
+        [TestMethod]
+        public void EncodeNestedEntry()
+        {
+            var root = new TestNode();
+            root.Children.Add(new TestNode
+            {
+                Name = "Client" , Children = { new TestNode("Windows") }
+            });
+            var res = root.Children.Flatten(
+                parent => parent.Children,
+                parent => parent.Name,
+                parent => parent.Data).Split('\n');
+
+            Assert.AreEqual("[Client]", res[0]);
+            Assert.AreEqual("[Client\\Windows]", res[1]);
+        }
+        [TestMethod]
+        public void EncodeTwoNestedEntries()
+        {
+            var root = new TestNode();
+            root.Children.Add(new TestNode
+            {
+                Name = "Client",
+                Children =
+                {
+                    new TestNode("Windows"), 
+                    new TestNode("Linux")
+                }
+            });
+            var res = root.Children.Flatten(
+                parent => parent.Children,
+                parent => parent.Name,
+                parent => parent.Data).Split('\n');
+
+            Assert.AreEqual("[Client]", res[0]);
+            Assert.AreEqual("[Client\\Linux]", res[1]);
+            Assert.AreEqual("[Client\\Windows]", res[2]);
+        }
+
+        [TestMethod]
+        public void EncodeFourNestedEntrys()
+        {
+            var root = new TestNode();
+            root.Children.Add(new TestNode
+            {
+                Name = "Client",
+                Children =
+                {
+                    new TestNode("Windows"),
+                    new TestNode("Linux")
+                }
+            });
+            root.Children.Add(new TestNode
+            {
+                Name = "Lib",
+                Children =
+                {
+                    new TestNode("Logic"),
+                    new TestNode("Common")
+                }
+            });
+            var res = root.Children.Flatten(
+                parent => parent.Children,
+                parent => parent.Name,
+                parent => parent.Data).Split('\n');
+
+            Assert.AreEqual("[Client]", res[0]);
+            Assert.AreEqual("[Client\\Linux]", res[1]);
+            Assert.AreEqual("[Client\\Windows]", res[2]);
+            Assert.AreEqual("[Lib]", res[3]);
+            Assert.AreEqual("[Lib\\Common]", res[4]);
+            Assert.AreEqual("[Lib\\Logic]", res[5]);
+        }
+
+        [TestMethod]
+        public void AlphabeticSort()
+        {
+            var root = new TestNode();
+            root.Children.Add(new TestNode
+            {
+                Name = "AA",
+                Children =
+                {
+                    new TestNode("EE"),
+                    new TestNode("FF")
+                }
+            });
+            root.Children.Add(new TestNode
+            {
+                Name = "BB",
+                Children =
+                {
+                    new TestNode("DD"),
+                    new TestNode("CC")
+                }
+            });
+            var res = root.Children.Flatten(
+                parent => parent.Children,
+                parent => parent.Name,
+                parent => parent.Data).Split('\n');
+
+            Assert.AreEqual("[AA]", res[0]);
+            Assert.AreEqual("[AA\\EE]", res[1]);
+            Assert.AreEqual("[AA\\FF]", res[2]);
+            Assert.AreEqual("[BB]", res[3]);
+            Assert.AreEqual("[BB\\CC]", res[4]);
+            Assert.AreEqual("[BB\\DD]", res[5]);
+        }
+    }
+}
